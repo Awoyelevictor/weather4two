8bc6fd5cb319e6cd920570396d3de2ac4c5893cf
Awoyele Emmanuel <awoyeleemma1@gmail.com>
2025-08-14 11:54:07 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/lib/weather.ts
+++ src/lib/weather.ts
@@ -3,9 +3,9 @@ import type {WeatherData} from './types';
 export const getWeatherData = async (
   locationName: string
 ): Promise<WeatherData> => {
-  const apiKey = process.env.WEATHER_API_KEY;
+  const apiKey = process.env.NEXT_PUBLIC_WEATHER_API_KEY;
   if (!apiKey) {
-    throw new Error('Missing WEATHER_API_KEY environment variable.');
+    throw new Error('Missing NEXT_PUBLIC_WEATHER_API_KEY environment variable.');
   }
   const response = await fetch(`https://api.weatherapi.com/v1/forecast.json?key=${apiKey}&q=${locationName}&days=7&aqi=no&alerts=no`);
   if (!response.ok) {
